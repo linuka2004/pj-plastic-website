b04c256a113db42e1f415bf431a383fd06e1cfcd
NimeshDK <it24103463@my.sliit.lk>
2025-10-22 15:53:49 +0530

    Add Order Management Backend

---
+++ pjplastic-backend/src/main/java/com/pjplastic/pjplastic_backend/controller/CreateOrderDto.java
@@ -0,0 +1,86 @@
+package com.pjplastic.pjplastic_backend.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Getter
+@Setter
+public class CreateOrderDto {
+    
+    private String orderName;
+    
+    private LocalDate orderDate;
+    
+    private LocalDate deliveryDate;
+
+    private Long userId;
+    private String senderName;
+    private String senderMobile;
+    private String deliveryAddress;
+    private String paymentMethod;
+    
+    private List<Item> items;
+
+    public String getOrderName() {
+        return orderName;
+    }
+
+    public void setOrderName(String orderName) {
+        this.orderName = orderName;
+    }
+
+    public LocalDate getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(LocalDate orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public LocalDate getDeliveryDate() {
+        return deliveryDate;
+    }
+
+    public void setDeliveryDate(LocalDate deliveryDate) {
+        this.deliveryDate = deliveryDate;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+    public String getSenderName() {
+        return senderName;
+    }
+    public void setSenderName(String senderName) {
+        this.senderName = senderName;
+    }
+    public String getSenderMobile() {
+        return senderMobile;
+    }
+    public void setSenderMobile(String senderMobile) {
+        this.senderMobile = senderMobile;
+    }
+    public String getDeliveryAddress() {
+        return deliveryAddress;
+    }
+    public void setDeliveryAddress(String deliveryAddress) {
+        this.deliveryAddress = deliveryAddress;
+    }
+    public String getPaymentMethod() { return paymentMethod; }
+    public void setPaymentMethod(String paymentMethod) { this.paymentMethod = paymentMethod; }
+    
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Item> items) {
+        this.items = items;
+    }
+}
